1c4bdaaca32c316bb54f34899328fd5caff35589
MrZento <160261326+MrZento@users.noreply.github.com>
2025-04-15 14:55:51 +0000

    hé

--- src/structures/DatabaseProvider.ts
+++
@@ -1,8 +0,0 @@
-abstract class DatabaseProvider {
-    abstract findUser(query: any);
-    abstract findSuspendedUsers();
-    abstract findBannedUsers();
-    abstract updateUser(query: any, data: any);
-}
-
-export { DatabaseProvider };
